61e5ca05574af0b1cdd0aa10c9ba94fcc99c4e84
Paroisse NDA <Alimagnim282@gmail.com>
2026-02-24 23:45:47 +0000

    Add files via upload
    mise a jour

--- script.js
+++ script.js
@@ -128,3 +128,16 @@ document.addEventListener('DOMContentLoaded', () => {
     initCompteur();
     initDashboard();
 });
+// Fonction pour ouvrir/fermer la barre latérale sur mobile
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    
+    if (sidebar.classList.contains('-translate-x-full')) {
+        sidebar.classList.remove('-translate-x-full');
+        overlay.classList.remove('hidden');
+    } else {
+        sidebar.classList.add('-translate-x-full');
+        overlay.classList.add('hidden');
+    }
+}
